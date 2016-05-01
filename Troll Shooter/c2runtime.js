f0f361b04600972496834b7e78f9d177ae1c9582
rohanssrao <rohanssrao@gmail.com>
2016-05-01 13:38:11 -0400

    made game 16:9; added app icons; lots and lots more

--- Troll Shooter/c2runtime.js
+++ Troll Shooter/c2runtime.js
@@ -19863,13 +19863,13 @@ cr.behaviors.scrollto = function(runtime)
 	behaviorProto.acts = new Acts();
 }());
 cr.getObjectRefTable = function () { return [
-	cr.plugins_.Keyboard,
 	cr.plugins_.Mouse,
+	cr.plugins_.Keyboard,
+	cr.plugins_.Text,
+	cr.plugins_.Sprite,
 	cr.plugins_.Touch,
 	cr.plugins_.Rex_Pause,
-	cr.plugins_.Text,
 	cr.plugins_.TiledBg,
-	cr.plugins_.Sprite,
 	cr.behaviors.EightDir,
 	cr.behaviors.scrollto,
 	cr.behaviors.bound,
